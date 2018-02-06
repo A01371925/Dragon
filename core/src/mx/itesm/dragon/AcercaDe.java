bd1c0efc26dff11b53235bf25c64f8b641aab3e7
LEDNR <A01371925@itesm.mx>
2018-02-06 14:07:39 -0600

    Clases ventanas.

---
+++ core/src/mx/itesm/dragon/AcercaDe.java
@@ -0,0 +1,14 @@
+package mx.itesm.dragon;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Created by LEDNR on 06/02/18.
+ */
+
+public class AcercaDe extends Game{
+    @Override
+    public void create() {
+
+    }
+}
--- core/src/mx/itesm/dragon/Principal.java
+++ core/src/mx/itesm/dragon/Principal.java
@@ -1,12 +1,12 @@
 package mx.itesm.dragon;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Principal extends ApplicationAdapter {
+public class Principal extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
