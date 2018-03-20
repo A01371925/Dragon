80abeb51cb36d77a86a128f5ed4f11a8825229a5
Jorge <A01373474@itesm.mx>
2018-03-20 14:00:30 -0600

    Transpaarencia Pausa

--- core/src/mx/itesm/dragon/Pantallas/PJP.java
+++ core/src/mx/itesm/dragon/Pantallas/PJP.java
@@ -12,12 +12,14 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 import java.util.ArrayList;
 
+import mx.itesm.dragon.Estados.Estado;
 import mx.itesm.dragon.Juego;
 import mx.itesm.dragon.Objetos.Fondo;
 import mx.itesm.dragon.Objetos.Pantalla;
@@ -28,63 +30,99 @@ public class PJP extends Pantalla {
 
     private final Juego juego;
 
-    private ArrayList<Proyectil> listaProyectil;
-
     private static final float ALTO_MAPA = 2560;
 
     private Stage stageJuego;
+    private Stage stagePausa;
 
     private Texture proyectil;
+
+    private ArrayList<Proyectil> listaProyectil;
 
     private Fondo fondo;
 
     private Image dragon;
-    private ImageButton btnRegresar;
+    private Image trasparencia;
+    private ImageButton btnPausa;
+    private ImageButton btnReanudar;
+
+    private Estado estado;
 
     private InputMultiplexer multiplexer;
 
     public PJP(Juego juego) {
         this.juego = juego;
+        estado = Estado.JUGANDO;
     }
 
     @Override
     public void show() {
-        cargarStage();
+        borrarPantalla();
+        inicializacion();
+        setStagePausa();
+        setStageJuego();
     }
 
-    private void cargarStage() {
+    private void inicializacion() {
+        // INICIALIZACIÃ“N DE COMPONENTES.
         multiplexer = new InputMultiplexer();
         listaProyectil = new ArrayList<Proyectil>();
         stageJuego = new Stage(vista);
+        stagePausa = new Stage(vista);
         fondo = new Fondo(new Texture("fondoNivel1.png"));
-        btnRegresar = new ImageButton(
+        btnPausa = new ImageButton(
+                new TextureRegionDrawable(new TextureRegion(
+                        new Texture("BotonRegresar.png"))));
+        btnReanudar = new ImageButton(
                 new TextureRegionDrawable(new TextureRegion(
                         new Texture("BotonRegresar.png"))));
         dragon = new Image(new Texture("Dragon.png"));
+
+
+
+        trasparencia = new Image(new Texture("fondoNegro.jpg"));
+        trasparencia.setColor(1,1,1,0.04f);
+
         proyectil = new Texture("BolaFuego.png");
 
-        btnRegresar.setPosition(0,ALTO - btnRegresar.getHeight());
-        dragon.setPosition(ANCHO / 2 - dragon.getWidth() / 2, 0);
+        // Se anexan las Escenas al Multiplexor.
+        multiplexer.addProcessor(stageJuego);
+        multiplexer.addProcessor(stagePausa);
+        Gdx.input.setInputProcessor(multiplexer);
+    }
 
-        btnRegresar.addListener(new ClickListener() {
+    public void setStagePausa() {
+
+        // PosisciÃ³n inicial de los elementos
+        trasparencia.setPosition(0,0);
+        btnReanudar.setPosition(0,ALTO - btnReanudar.getHeight() - btnReanudar.getHeight());
+
+        btnReanudar.addListener(new ClickListener() {
             @Override
-            public void clicked(InputEvent event, float x, float y) {
-                juego.setScreen(new PantallaMenuPrincipal(juego));
+            public boolean isPressed() {
+
+                return false;
             }
         });
 
+        // Se anexan los Actores a la Escena.
+        stagePausa.addActor(trasparencia);
+        stagePausa.addActor(btnReanudar);
+    }
 
+    public void setStageJuego() {
+
+        // Posisción inicial de los elementos
+        btnPausa.setPosition(0,ALTO - btnPausa.getHeight());
+        dragon.setPosition(ANCHO / 2 - dragon.getWidth() / 2, 0);
 
         dragon.addListener(new DragListener() {
-
-
-
             @Override
             public void touchDragged (InputEvent event, float x, float y, int pointer) {
                 // example code below for origin and position
                 Vector3 v = new Vector3(Gdx.input.getX(),Gdx.input.getY(),0);
                 camara.unproject(v);
-                if (v.y <= ALTO - btnRegresar.getHeight() - dragon.getImageHeight() / 2) {
+                if (v.y <= ALTO - btnPausa.getHeight() - dragon.getImageHeight() / 2) {
                     dragon.setPosition(v.x - dragon.getImageWidth() / 2, v.y - dragon.getImageHeight() / 2);
                 } else {
                     dragon.setX(v.x - dragon.getImageWidth() / 2);
@@ -92,50 +130,62 @@ public class PJP extends Pantalla {
             }
         });
 
-
-
-        stageJuego.addActor(btnRegresar);
+        // Se anexan los Actores a la Escena.
+        stageJuego.addActor(btnPausa);
         stageJuego.addActor(dragon);
 
-        multiplexer.addProcessor(stageJuego);
 
-        Gdx.input.setInputProcessor(multiplexer);
     }
 
     @Override
     public void render(float delta) {
-        borrarPantalla();
-        actualizarObjetos(delta);
-        actualizarCamara();
-        moverCamara();
-        batch.begin();
-            fondo.render(batch);
-            listaProyectil.add(new Proyectil(proyectil, dragon.getX() + dragon.getWidth() / 2 - proyectil.getWidth() / 2, dragon.getY() + dragon.getHeight()));
 
-        for (Proyectil p: listaProyectil) {
-                p.render(batch);
-            }
-        batch.end();
-        stageJuego.draw();
+        switch (estado) {
+            case JUGANDO:
+                actualizarObjetos(delta);
+                actualizarCamara();
+                moverCamara();
+                batch.begin();
+                fondo.render(batch);
+                for (Proyectil p: listaProyectil) {
+                    p.render(batch);
+                }
+                batch.end();
+                stageJuego.draw();
+
+                if (btnPausa.isPressed()) {
+                    estado = Estado.PAUSA;
+                }
+
+                break;
+            case PAUSA:
+                batch.begin();
+                batch.end();
+                stagePausa.draw();
+                if (btnReanudar.isPressed()) {
+                    estado = Estado.JUGANDO;
+                }
+                break;
+        }
     }
 
     private void actualizarCamara() {
-        // Depende de la posición del personaje. Siempre sigue al personaje
+        // Depende de la posiciÃ³n del personaje. Siempre sigue al personaje
         float posY = dragon.getImageY();
         // Primera mitad de la pantalla.
         if (posY < ALTO/2 ) {
             camara.position.set(ANCHO / 2, ALTO / 2, 0);
-        } else if (posY > ALTO_MAPA - ANCHO / 2) {   // Última mitad de la pantalla
+        } else if (posY > ALTO_MAPA - ANCHO / 2) {   // Ãšltima mitad de la pantalla
             camara.position.set(camara.position.x,ALTO_MAPA - ANCHO/2,0);
-        } /*else if (posY < ALTO_MAPA - ANCHO / 2){    // En 'medio' del mapa
-            camara.position.set(camara.position.x, posY,0);
-        }*/
+        }
         camara.update();
     }
 
     private void actualizarObjetos(float delta) {
+        listaProyectil.add(new Proyectil(proyectil, dragon.getX() + dragon.getWidth() / 2 - proyectil.getWidth() / 2, dragon.getY() + dragon.getHeight()));
+
         for (int i = 0; i < listaProyectil.size(); i++) {
-            listaProyectil.get(i).mover(delta);
+            listaProyectil.get(i).mover(delta - 10);
         }
 
         for (int i = 0; i < listaProyectil.size(); i++) {
@@ -143,26 +193,24 @@ public class PJP extends Pantalla {
                 listaProyectil.remove(i);
             }
         }
-
-        System.out.println(listaProyectil.size());
-
         fondo.mover(delta);
-
     }
+
 
     @Override
     public void pause() {
-
+        estado = Estado.PAUSA;
     }
 
     @Override
     public void resume() {
-
+        estado = Estado.PAUSA;
     }
 
     @Override
     public void dispose() {
         stageJuego.dispose();
+        stagePausa.dispose();
         proyectil.dispose();
     }
 }
