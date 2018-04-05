9427173c7128f091f6e1fee8f17e07b226fba9dd
LEDNR <A01371925@itesm.mx>
2018-04-04 22:18:22 -0500

    Clase Animacion.

---
+++ core/src/mx/itesm/dragon/Objetos/Animacion.java
@@ -0,0 +1,33 @@
+package mx.itesm.dragon.Objetos;
+
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Animacion {
+
+    private Texture textura;
+    private TextureRegion[] frames;
+    private Animation animacion;
+    private int tileWidth, tileHeight, textureRegion;
+    private float speed;
+
+    public Animacion(Texture textura, int tileWidth, int tileHeight, int textureRegion, float speed) {
+        this.textura = textura;
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+        this.textureRegion = textureRegion;
+        this.speed = speed;
+    }
+
+    public Animation animacion() {
+        TextureRegion[][] tmp = TextureRegion.split(textura, tileWidth, tileHeight);
+        frames = new TextureRegion[textureRegion];
+        for (int i = 0; i < frames.length; i++) {
+            frames[i] = tmp[0][i];
+        }
+        animacion = new Animation<>(speed, frames);
+        return animacion;
+    }
+}
