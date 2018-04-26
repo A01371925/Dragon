ab9cb57ed9305936ce8135c6c39642e040b596d2
LEDNR <A01371925@itesm.mx>
2018-04-26 14:39:18 -0500

    Correccion inputprocessor

--- core/src/mx/itesm/dragon/Levels/LevelOne.java
+++ core/src/mx/itesm/dragon/Levels/LevelOne.java
@@ -3,7 +3,6 @@ package mx.itesm.dragon.Levels;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import java.util.ArrayList;
 import java.util.Random;
